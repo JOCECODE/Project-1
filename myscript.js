29ff5ade9e4408f48ecbd77156a6df90f8aa9529
JOCECODE <66341350+JOCECODE@users.noreply.github.com>
2020-08-20 02:50:33 -0700

    added on hover

--- myscript.js
+++ myscript.js
@@ -165,8 +165,6 @@ $(document).ready(function (event) {
       number: t.rest.phoneNumber,
     };
     localStorage.setItem("thisRestObj", JSON.stringify(restObj));
-    console.log(restObj);
-
     window.location.href = "index3.html";
   });
 });
@@ -174,7 +172,6 @@ $(document).ready(function (event) {
 // DISPLAY MARKER ON GOOGLE MAPS 1 ON INDEX2.HTML
 function displayMarker() {
   var local = JSON.parse(localStorage.getItem("response"));
-  console.log(local);
   for (var i = 0; i < local.restaurants.length; i++) {
     var lat = local.restaurants[i].restaurant.location.latitude;
     var lng = local.restaurants[i].restaurant.location.longitude;
@@ -244,7 +241,6 @@ function renderItems() {
     all.times = times;
     all.long = long;
     all.lat = lat;
-    console.log(all);
     var divEl = $("<h4>");
     var pAddress = $("<p>", { class: "newAdd" });
     var pNumber = $("<p>", { class: "newNumber" });
@@ -254,16 +250,25 @@ function renderItems() {
     divEl.text(restName).appendTo($("#results"));
     pAddress.text(address).appendTo($("#results"));
     pNumber.text(phoneNumber).appendTo($("#results"));
-    // $(".address").attr("data-rest", JSON.stringify(all));
-    // $(".phoneNumber").attr("data-rest", JSON.stringify(all));
-    // $("<p>", { class: "address" }).text(address).appendTo($("#results"));
-    // $("<p>", { class: "phoneNumber" })
-    // .text(phoneNumber)
-    // .appendTo($("#results"));
-
     $("<br>").appendTo($("#results"));
     localStorage.setItem("restName" + [i], JSON.stringify(all));
   }
 }
 renderItems();
 renderThisRestObj();
+
+// ON HOVER FUNCTION FOR INDEX 2 HTML
+$("h4, .newAdd, .newNumber").on({
+  mouseenter: function () {
+    $(this).css("background-color", "black");
+    $(this).css("opacity", 0.6);
+    $(this).css("padding", "8px");
+    $(this).css("transition", ".5s");
+  },
+  mouseleave: function () {
+    $(this).css("background-color", "transparent");
+    $(this).css("opacity", 1);
+    $(this).css("transition", ".5s");
+    $(this).css("padding", "0px");
+  },
+});
